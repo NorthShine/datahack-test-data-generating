8babe11fa11b696af05314a984ba883007a31c90
scripthound <grinkiko@gmail.com>
2022-08-06 02:35:17 +0300

    [SETUP.PY] altered project structure so one is now able to build it

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name='no_spark_in_my_home',
+    version='0.1.0',
+    setup_requires=['wheel'],
+    packages=find_packages(include=['src', 'src.*'])
+)
